75982c64803bfa6af776d22ab1c175e2506bb6e5
Leo Joseph Buchignani III <Leo.Little.Book@Gmail.com>
2016-10-17 16:48:24 +0800

    cleanup hyper keybind test

--- hyper-keybind-test.el
+++
@@ -1,9 +0,0 @@
-;; from http://ergoemacs.org/emacs/emacs_menu_app_keys.html
-
-(define-key key-translation-map (kbd "<apps>") (kbd "<menu>"))
-
-(progn
-  (define-prefix-command 'xah-fly-leader-key-map))
-
-  
-(global-set-key (kbd "<menu>") 'xah-fly-leader-key-map)
