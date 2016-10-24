6beeca5ee93ad593ae747c446a4595a2dd2b624c
Leo Joseph Buchignani III <Leo.Little.Book@gmail.com>
2016-10-24 20:18:33 +0800

    added org hooks to keybinds

--- packages.el
+++ packages.el
@@ -1,13 +1,7 @@
 ;;; packages.el --- personal layer packages file for Spacemacs.
 ;;
-;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
-;;
-;; Author:  <User@rog>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
 ;; This file is not part of GNU Emacs.
 ;;
-;;; License: GPLv3
 
 ;;; Commentary:
 
