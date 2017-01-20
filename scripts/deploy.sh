12dc251bfa4b862638ff2fd7d0b467826473b47a
Texas Cyberthal <texas.cyberthal@gmail.com>
2017-01-20 14:26:43 +0800

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,9 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -f ~/.spacemacs
-ln -s $SCRIPT_DIR/.spacemacs ~/
+ln -s $SCRIPT_DIR/../.spacemacs ~/
 
 rm -rf ~/.emacs.d/private/personal
-ln -s $SCRIPT_DIR ~/.emacs.d/private/personal
+ln -s $SCRIPT_DIR/../ ~/.emacs.d/private/personal
 
 cp maintext-toggle.el.bak ../maintext-toggle.el
