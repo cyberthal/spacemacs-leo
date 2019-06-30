682d8976a76db24deda4af2b5e0c01457ea5c184
leo3 <texas.cyberthal@gmail.com>
2019-07-01 00:48:42 +0800

    changed Textmind top-level schema to reflect Org Agenda. Fixed deploy.

--- scripts/Deploy/not-windows.sh
+++ scripts/Deploy/not-windows.sh
@@ -3,10 +3,12 @@
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && cd ../../ && pwd )"
 
 LINK_NAME=${SCRIPT_DIR##*/}
-link_name=$(echo "$LINK_NAME" | tr '[:upper:]' '[:lower:]')
+# to follow Spacemacs convention, layer directories should be lowercase 
 
 ln -sf $SCRIPT_DIR/.spacemacs ~/
 
-ln -sf $SCRIPT_DIR/ ~/.emacs.d/private/$link_name
+rm ~/.emacs.d/private/$LINK_NAME
+# without the rm, the ln will inexplicably link at SCRIPT_DIR if target already exists.
+ln -sf $SCRIPT_DIR/ ~/.emacs.d/private/$LINK_NAME
 
 cp -f $SCRIPT_DIR/scripts/maintext-toggle.el.bak $SCRIPT_DIR/maintext-toggle.el
