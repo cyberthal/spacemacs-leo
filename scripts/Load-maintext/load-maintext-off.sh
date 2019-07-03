dca2fea4270f3edc34e932a2e062391d581ca6b6
leo3 <texas.cyberthal@gmail.com>
2019-07-03 16:37:13 +0800

    fixed trailing slashes. deleted maintext toggle scripts.

--- scripts/Load-maintext/load-maintext-off.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-perl -pi -e 's/load-org-agenda-files-recursively/print/g' ../../maintext-toggle.el
--- scripts/Load-maintext/load-maintext-on.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-perl -pi -e 's/print "~/load-org-agenda-files-recursively "~/g' ../../maintext-toggle.el
