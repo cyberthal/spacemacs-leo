f4ae2f4ecd51551da410ba63e623a9973374dd86
leo3 <texas.cyberthal@gmail.com>
2019-07-11 20:06:00 +0800

    add dired+ download to deploy script

--- deploy-nixlike.sh
+++ deploy-nixlike.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# download dired+ to spacemacs dir. clunky temporary workaround. [2019-07-11]
+mkdir -p ~/.emacs.d/private/local/dired-plus
+wget -m https://www.emacswiki.org/emacs/download/dired%2b.el -O ~/.emacs.d/private/local/dired-plus/dired+.el
+
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 
 LINK_NAME=${SCRIPT_DIR##*/}
