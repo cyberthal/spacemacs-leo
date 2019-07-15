d7d1b2149255ac0e4221f7162c752ebfaa767e78
leo3 <texas.cyberthal@gmail.com>
2019-07-15 18:43:26 +0800

    hardcode repo name in deploy script.

--- deploy-nixlike.sh
+++ deploy-nixlike.sh
@@ -6,11 +6,10 @@ wget -m https://www.emacswiki.org/emacs/download/dired%2b.el -O ~/.emacs.d/priva
 
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 
-LINK_NAME=${SCRIPT_DIR##*/}
-link_name=$(echo "$LINK_NAME" | tr '[:upper:]' '[:lower:]')
+LINK_NAME="personal"
 
 ln -sf $SCRIPT_DIR/.spacemacs ~/
 
-rm ~/.emacs.d/private/$link_name
+rm ~/.emacs.d/private/$LINK_NAME
 # without the rm, the ln will inexplicably link at SCRIPT_DIR if target already exists.
-ln -sf $SCRIPT_DIR/ ~/.emacs.d/private/$link_name
+ln -sf $SCRIPT_DIR/ ~/.emacs.d/private/$LINK_NAME
