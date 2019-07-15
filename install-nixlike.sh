ca163c6332cca8362e7266918ac4e6bf96f3f54b
leo3 <texas.cyberthal@gmail.com>
2019-07-15 20:49:04 +0800

    adapt install.sh to chemacs paths

---
+++ install-nixlike.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# * offset
+
+# ** download dired+
+
+# download dired+ to spacemacs dir. clunky temporary workaround. [2019-07-11]
+mkdir -p ~/.emacs.d/private/local/dired-plus
+wget -m https://www.emacswiki.org/emacs/download/dired%2b.el -O ~/.emacs.d/private/local/dired-plus/dired+.el
+
+# ** symlinks
+
+# find path to repo
+SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
+
+# link .init.el
+rm -f ~/.spacemacs.d/.init.el
+mkdir ~/.spacemacs.d/
+ln -s $SCRIPT_DIR/.init.el ~/.spacemacs.d/
+
+# link personal directory
+LINK_NAME="personal"
+rm -f ~/.spacemacs.d/$LINK_NAME
+ln -s $SCRIPT_DIR/ ~/.spacemacs.d/$LINK_NAME
