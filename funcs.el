8697e79f03e638e806b2273fd013642b37cf8882
leo3 <texas.cyberthal@gmail.com>
2019-07-19 19:10:36 +0800

    gained a function from treesort

--- funcs.el
+++ funcs.el
@@ -9,3 +9,23 @@
   (interactive)
   (org-insert-time-stamp (current-time) t t)
   )
+
+;; ** ts-checklist-to-not-done
+
+(defun ts-checklist-to-not-done
+    ()
+  "Replaces all checklist X with SPACE"
+  (interactive)
+
+  (save-excursion
+    (while (search-forward "[X]" nil t)
+      (replace-match "[ ]" nil t))
+    )
+  )
+
+;; Org-Mode: "boxes" Advance down org-mode checklist with C-S-n
+(fset 'boxes
+      (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("" 0 "%d")) arg)))
+;; binding it to a handy key, since as a command it won't macro repeat
+(global-set-key (kbd "C-S-n") 'boxes)
+
