531f38a51ade45f1cd786f44911bd558ce9767a2
leo3 <texas.cyberthal@gmail.com>
2019-07-19 19:02:38 +0800

    traded functions with treesort

--- funcs.el
+++ funcs.el
@@ -1,23 +1,11 @@
 ;; * funcs.el in private/personal layer
 
-;; ** C-c k QUICK KILL
+;; ** Insert inactive timestamp of current time
 
-;; faster scratch file deletion - defining the function
-(defun delete-this-buffer-and-file ()
-  "Removes file connected to current buffer and kills buffer."
+(defun ts-org-time-and-date-stamp-inactive () 
+  "Insert inactive timestamp of current time"
+
+  ;; Calls org-time-stamp-inactive with universal prefix
   (interactive)
-  (let ((filename (buffer-file-name))
-        (buffer (current-buffer))
-        (name (buffer-name)))
-    (if (not (and filename (file-exists-p filename)))
-        (error "Buffer '%s' is not visiting a file!" name)
-      (when (yes-or-no-p "Are you sure you want to remove this file? ")
-        (delete-file filename)
-        (kill-buffer buffer)
-        (message "File '%s' successfully removed" filename)))))
-
-;; bound function to C-c k
-(global-set-key (kbd "C-c k") 'delete-this-buffer-and-file) 
-
-
-
+  (org-insert-time-stamp (current-time) t t)
+  )
