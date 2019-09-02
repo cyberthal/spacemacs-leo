5573e14b6f94db471b72d725b45e6f34f47a8fd7
leo3 <texas.cyberthal@gmail.com>
2019-09-02 22:05:10 +0800

    add anzu

--- init.el
+++ init.el
@@ -61,7 +61,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(lispy dired-du org-make-toc bbcode-mode magit-annex git-annex dired-quick-sort outshine flycheck-package)
+   dotspacemacs-additional-packages '(anzu lispy dired-du org-make-toc bbcode-mode magit-annex git-annex dired-quick-sort outshine flycheck-package)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
