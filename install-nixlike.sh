ace7a2ed4fd3bac46144a25a9b009ed82f6a356e
demo <bibliodemos@gmail.com>
2019-11-28 01:20:23 +0800

    deploy persp-mode Textmind-demo file

--- install-nixlike.sh
+++ install-nixlike.sh
@@ -18,8 +18,10 @@ ln -s $SCRIPT_DIR/ ~/.emacs.d/private/personal
 
 # symlink persp-mode Textmind layout
 rm -f ~/.emacs.d/.cache/layouts/Textmind-main
+rm -f ~/.emacs.d/.cache/layouts/Textmind-demo
 mkdir -p ~/.emacs.d/.cache/layouts/
 ln -s $SCRIPT_DIR/persp-mode/Textmind-main ~/.emacs.d/.cache/layouts/
+ln -s $SCRIPT_DIR/persp-mode/Textmind-demo ~/.emacs.d/.cache/layouts/
 
 # remove default .spacemacs
 mv -f ~/.spacemacs ~/.spacemacs-bak
