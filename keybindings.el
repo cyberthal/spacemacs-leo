e25fa16c90d0bbcb6d0ccba4dd38fdaabdc32700
leo3 <texas.cyberthal@gmail.com>
2019-12-15 14:21:48 +0800

    add leo-org-refactor-heading keybind

--- keybindings.el
+++ keybindings.el
@@ -22,6 +22,7 @@
 (global-set-key (kbd "C-c k") 'leo-delete-this-buffer-and-file)
 (global-set-key (kbd "s-n") 'leo-pipify-word-list)
 (global-set-key (kbd "C-c l") 'leo-org-store-link-fold-drawer)
+(global-set-key (kbd "H-v") 'leo-org-refactor-heading)
 
 ;; ** org-mode
 
