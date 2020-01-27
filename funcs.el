2d1c0ec9681a5762bdb7fb8a51d37c9ff9ea8f7f
leo3 <texas.cyberthal@gmail.com>
2020-01-27 13:38:07 +0800

    comment out broken Textmind funcs

--- funcs.el
+++ funcs.el
@@ -10,6 +10,7 @@
         ((member "Georgia" (font-family-list)) (set-face-font 'variable-pitch "Georgia"))
         ((member "aakar" (font-family-list)) (set-face-font 'variable-pitch "aakar"))
         ((member "Times New Roman" (font-family-list)) (set-face-font 'variable-pitch "Times New Roman"))))
+
 ;; ** Linux xmodmap trigger
 
 (defun leo-xmodmap ()
@@ -56,67 +57,67 @@
 
 ;; ***** cut
 
-(defun leo-cut-prior-sprinting ()
-  "File prior sprinting heading twice to '2sprinted.org"
+;; (defun leo-cut-prior-sprinting ()
+;;   "File prior sprinting heading twice to '2sprinted.org"
 
-  (set-buffer "'1sprinting.org")
-  (widen)
-  (goto-char (point-min))
-  (if (org-at-heading-p)
-      ()
-    (org-next-visible-heading 1))
-  (assert (string-equal "* "
-           (buffer-substring (point) (+ (point) 2)))
-          nil "Buffer should begin with a one-star headline")
+;;   (set-buffer "'1sprinting.org")
+;;   (widen)
+;;   (goto-char (point-min))
+;;   (if (org-at-heading-p)
+;;       ()
+;;     (org-next-visible-heading 1))
+;;   (assert (string-equal "* "
+;;            (buffer-substring (point) (+ (point) 2)))
+;;           nil "Buffer should begin with a one-star headline")
 
-  (org-narrow-to-subtree)
-  (org-global-cycle)
-  (org-demote-subtree)
-  (org-demote-subtree)
-  (org-demote-subtree)
-  (trs-snort-visible)
+;;   (org-narrow-to-subtree)
+;;   (org-global-cycle)
+;;   (org-demote-subtree)
+;;   (org-demote-subtree)
+;;   (org-demote-subtree)
+;;   (trs-snort-visible)
 
-  ;; is this necessary?
-  (goto-char (point-max))
-  )
+;;   ;; is this necessary?
+;;   (goto-char (point-max))
+;;   )
 
 ;; ***** paste
 
-(defun leo-double-file-prior-sprinting ()
-  "File prior sprinting heading twice to '2sprinted.org"
+;; (defun leo-double-file-prior-sprinting ()
+;;   "File prior sprinting heading twice to '2sprinted.org"
 
-    (leo-cut-prior-sprinting)
+;;     (leo-cut-prior-sprinting)
 
-    (assert (file-exists-p "~/1-Mansort/1-Textmind/2-Linked/8-Hud/'2sprinted.org"))
-    (find-file "~/1-Mansort/1-Textmind/2-Linked/8-Hud/'2sprinted.org")
+;;     (assert (file-exists-p "~/1-Mansort/1-Textmind/2-Linked/8-Hud/'2sprinted.org"))
+;;     (find-file "~/1-Mansort/1-Textmind/2-Linked/8-Hud/'2sprinted.org")
 
-    (goto-char (point-min))
-    (word-search-forward "Log")
-    (org-show-all '(headings))
-    (org-cycle)
-    (org-cycle)
-    (org-forward-heading-same-level 1)
-    (insert "\n")
-    (backward-char)
-    (insert trs-object-text)
-    (insert "\n")
-    (save-buffer)
-    (let ((trs-searched-file-path (concat default-directory "../Inbox.org")))
-      (trs-create-inbox-org)
-        )
-    (trs-insert-to-end-of-buffer)
-    (save-buffer)
-    (switch-to-buffer "'Meta.org")
-  )
+;;     (goto-char (point-min))
+;;     (word-search-forward "Log")
+;;     (org-show-all '(headings))
+;;     (org-cycle)
+;;     (org-cycle)
+;;     (org-forward-heading-same-level 1)
+;;     (insert "\n")
+;;     (backward-char)
+;;     (insert trs-object-text)
+;;     (insert "\n")
+;;     (save-buffer)
+;;     (let ((trs-searched-file-path (concat default-directory "../Inbox.org")))
+;;       (trs-create-inbox-org)
+;;         )
+;;     (trs-insert-to-end-of-buffer)
+;;     (save-buffer)
+;;     (switch-to-buffer "'Meta.org")
+;;   )
 ;; **** deletion pass
 
-(defun leo-proc-sprinted-deletion-pass ()
-    "Setup other frame to begin deletion pass for proc sprinted checklist."
+;; (defun leo-proc-sprinted-deletion-pass ()
+;;     "Setup other frame to begin deletion pass for proc sprinted checklist."
 
-  (other-frame 1)
-  (delete-other-windows)
-  (assert (file-exists-p "~/1-Mansort/1-Textmind/2-Linked/Inbox.org"))
-  (find-file "~/1-Mansort/1-Textmind/2-Linked/Inbox.org")
-  (org-show-all)
-  (goto-char (point-max))
-  )
+;;   (other-frame 1)
+;;   (delete-other-windows)
+;;   (assert (file-exists-p "~/1-Mansort/1-Textmind/2-Linked/Inbox.org"))
+;;   (find-file "~/1-Mansort/1-Textmind/2-Linked/Inbox.org")
+;;   (org-show-all)
+;;   (goto-char (point-max))
+;;   )
